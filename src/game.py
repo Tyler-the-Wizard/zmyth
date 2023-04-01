601ccd279bad4ea779958319db0f7cf0de78a193
Tyler Starr <tyler.starr413@gmail.com>
2023-03-31 23:45:13 -0600

    Update gameplay aspects

--- src/game.py
+++ src/game.py
@@ -2,7 +2,7 @@ import color
 
 class Game:
     def __init__(self):
-        self.items = []
+        self.items = [Item('Gravel', -1)]
         self.tools = [Tool('hands', 0)]
         self.machines = []
 
@@ -12,7 +12,18 @@ class Item:
         self.amount = amount
 
     def __str__(self):
-        return f'{self.name} x{self.amount}'
+        amount_str = color.PURPLE('Inf') if self.amount < 0 else str(self.amount)
+        return f'{self.name} x{amount_str}'
+
+    def __add__(self, lhs):
+        if self.amount >= 0:
+            self.amount += lhs
+
+    def __sub__(self, lhs):
+        if self.amount >= 0:
+            if lhs > self.amount:
+                raise ArithmeticError
+            self.amount -= lhs
 
 class Tool:
     def __init__(self, name, level):
