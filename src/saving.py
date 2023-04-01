ffa510fa80c2c42ee9bd0c76e6409ba165c21723
Tyler Starr <tyler.starr413@gmail.com>
2023-04-01 14:57:22 -0600

    Got saving + loading working

--- src/saving.py
+++ src/saving.py
@@ -1,4 +1,5 @@
 import os
+import zlib
 
 import constants
 import game
@@ -10,7 +11,9 @@ def get_path(save):
         save)
 
 def save():
-    data = '!Items\n'
+    data = ''
+
+    data += '!Items\n'
     for item in settings.GAME.items:
         data += item.name + ':' + str(item.amount) + '\n'
 
@@ -22,33 +25,48 @@ def save():
     for machine in settings.GAME.machines:
         data += machine.name + ':' + str(machine.level) + '\n'
 
-    with open(get_path(settings.SAVE_NAME), 'w') as file:
-        file.write(data)
+    with open(get_path(settings.SAVE_NAME), 'wb') as file:
+        file.write(zlib.compress(data.encode()))
 
 def parse_category(category):
-    return [entry for entry in category.split('\n'[1:] if entry)]
+    return [entry for entry in category[1:] if entry]
 
 def parse_data(data):
-    for category in data:
+    for category in [c.split('\n') for c in data if c]:
         if category[0] == 'Items':
-            items = parse_category(category)
+            items_str = parse_category(category)
+        elif category[0] == 'Tools':
+            tools_str = parse_category(category)
+        elif category[0] == 'Machines':
+            machines_str = parse_category(category)
+
+    items = []
+    tools = []
+    machines = []
+
+    try:
+        for s in items_str:
+            item_dat = s.split(':')
+            items.append(game.Item(item_dat[0], int(item_dat[1])))
+        for s in tools_str:
+            tool_dat = s.split(':')
+            tools.append(game.Tool(tool_dat[0], int(tool_dat[1])))
+        for s in machines_str:
+            machine_dat = s.split(':')
+            machines.append(game.Machine(machine_dat[0], int(machine_dat[1])))
+    except UnboundLocalError:
+        print(color.RED('Error:'), 'unable to load save')
+        raise
+
+    return items, tools, machines
 
 def load(filename):
-    with open(get_path(filename)) as file:
-        data = file.read()
+    with open(get_path(filename), 'rb') as file:
+        data = zlib.decompress(file.read()).decode()
 
     data = data.split('!')
 
     items, tools, machines = parse_data(data)
-
-    items = [item for item in data[0].split('\n')[1:] if item]
-    print(items)
-
-    tools = [tool for tool in data[1].split('\n')[1:] if tool]
-    print(tools)
-
-    machines = [machine for machine in data[2].split('\n')[1:] if machine]
-    print(machines)
 
     settings.GAME = game.Game()
     settings.GAME.items = items
